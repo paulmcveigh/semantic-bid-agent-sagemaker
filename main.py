412b30c7c6d3bbb8402ec4407b9986fd9f65f441
paulmcveigh <paul.mcveigh@kainos.com>
2025-07-28 14:46:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -145,52 +145,52 @@ class RiskEvaluator:
             "model_used": "fraud-detection-xgb-v1-endpoint"
         }
 
-        '''return {
-            "risk_score": 0.48,
-            "service_used": self.runtime,
-            "model_used": self.endpoint_name 
-        }'''
-        
-        '''coverage_amount = claim_data.get("coverage_amount", "")
-        
-        region_of_operation = claim_data.get("region_of_operation", "").lower()
-        organisation_name =  claim_data.get("organisation_name", "").lower()
-        
-        if region_of_operation == "gb":
-            region_value = 0
-        elif region_of_operation == "usa":
-            region_value = 1
-        elif region_of_operation == "eu":
-            region_value = 2
-        elif region_of_operation == "asia":
-            region_value = 3
-        elif region_of_operation == "africa":
-            region_value = 4
-        else:
-            region_value = 5'''
-        '''
-        #payload = f"{coverage_amount},{region_value}"
-        payload = f"1,150000000"
+'''return {
+    "risk_score": 0.48,
+    "service_used": self.runtime,
+    "model_used": self.endpoint_name 
+}'''
+
+'''coverage_amount = claim_data.get("coverage_amount", "")
+
+region_of_operation = claim_data.get("region_of_operation", "").lower()
+organisation_name =  claim_data.get("organisation_name", "").lower()
+
+if region_of_operation == "gb":
+    region_value = 0
+elif region_of_operation == "usa":
+    region_value = 1
+elif region_of_operation == "eu":
+    region_value = 2
+elif region_of_operation == "asia":
+    region_value = 3
+elif region_of_operation == "africa":
+    region_value = 4
+else:
+    region_value = 5'''
+'''
+#payload = f"{coverage_amount},{region_value}"
+payload = f"1,150000000"
 
 
-        response = self.runtime.invoke_endpoint(
-            EndpointName=self.endpoint_name,
-            ContentType="text/csv",
-            Body=payload
-        )
-        prediction = json.loads(response["Body"].read().decode())
+response = self.runtime.invoke_endpoint(
+    EndpointName=self.endpoint_name,
+    ContentType="text/csv",
+    Body=payload
+)
+prediction = json.loads(response["Body"].read().decode())
 
-        return {
-            "risk_score": prediction
-        }
+return {
+    "risk_score": prediction
+}
 
-        rationale = f"Risk for organisation: {organisation_name} assessed as {prediction} based on our model."
+rationale = f"Risk for organisation: {organisation_name} assessed as {prediction} based on our model."
 
-        return {
-            "risk_score": prediction,
-            "explanation": rationale
-        }
-        '''
+return {
+    "risk_score": prediction,
+    "explanation": rationale
+}
+'''
 
 
 
@@ -222,46 +222,46 @@ class InsurancePremiumEstimator:
             "model_used": self.endpoint_name 
         }
 
-        '''coverage_amount = claim_data.get("coverage_amount", "")
-        region_of_operation = claim_data.get("region_of_operation", "").lower()
-        #coverage_amount_str = claim_data.get("coverage_amount", "").lower()
-        #region_of_operation = claim_data.get("region_of_operation", "").lower()
-        #cleaned = coverage_amount_str.lower().replace(",", "")
-        # Extract digits only
-        #digits = ''.join(filter(str.isdigit, cleaned))
-        # Convert to integer and scale down
-        coverage_amount = int(coverage_amount) // 1000 if coverage_amount else 0
-        
-        if region_of_operation == "gb":
-            region_value = 0
-        elif region_of_operation == "usa":
-            region_value = 1
-        elif region_of_operation == "eu":
-            region_value = 2
-        elif region_of_operation == "asia":
-            region_value = 3
-        elif region_of_operation == "africa":
-            region_value = 4
-        else:
-            region_value = 5
+'''coverage_amount = claim_data.get("coverage_amount", "")
+region_of_operation = claim_data.get("region_of_operation", "").lower()
+#coverage_amount_str = claim_data.get("coverage_amount", "").lower()
+#region_of_operation = claim_data.get("region_of_operation", "").lower()
+#cleaned = coverage_amount_str.lower().replace(",", "")
+# Extract digits only
+#digits = ''.join(filter(str.isdigit, cleaned))
+# Convert to integer and scale down
+coverage_amount = int(coverage_amount) // 1000 if coverage_amount else 0
 
-        payload = f"{coverage_amount},{region_value}"
+if region_of_operation == "gb":
+    region_value = 0
+elif region_of_operation == "usa":
+    region_value = 1
+elif region_of_operation == "eu":
+    region_value = 2
+elif region_of_operation == "asia":
+    region_value = 3
+elif region_of_operation == "africa":
+    region_value = 4
+else:
+    region_value = 5
 
-        #try:
-        response = self.runtime.invoke_endpoint(
-            EndpointName=self.endpoint_name,
-            ContentType="text/csv",
-            Body=payload
-        )
-        result = json.loads(response["Body"].read().decode())
-        prediction = result["predictions"][0]["score"]
+payload = f"{coverage_amount},{region_value}"
 
-        return {
-            "estimated_insurance_premium": round(prediction, 2),
-            "currency": "GBP",
-            "service_used": self.runtime,
-            "model_used": self.endpoint_name 
-        }'''
+#try:
+response = self.runtime.invoke_endpoint(
+    EndpointName=self.endpoint_name,
+    ContentType="text/csv",
+    Body=payload
+)
+result = json.loads(response["Body"].read().decode())
+prediction = result["predictions"][0]["score"]
+
+return {
+    "estimated_insurance_premium": round(prediction, 2),
+    "currency": "GBP",
+    "service_used": self.runtime,
+    "model_used": self.endpoint_name 
+}'''
 
 
 
