5ab27a74a37eeb0aa679a14761c395401859fc96
paulmcveigh <paul.mcveigh@kainos.com>
2025-08-01 15:48:09 +0100

    Update agent_text_processing.py

--- agent_text_processing.py
+++ agent_text_processing.py
@@ -1,3 +1,38 @@
+import asyncio
+import os
+import json
+import requests
+import faiss
+import numpy as np
+import sqlite3
+import streamlit as st
+import random
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+from dataclasses import dataclass, field, asdict
+from typing import Annotated, Optional, TypedDict, List, Any, Dict
+
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import openai
+
+from semantic_kernel import Kernel
+from semantic_kernel.agents import ChatCompletionAgent, ChatHistoryAgentThread
+from semantic_kernel.functions import KernelArguments, kernel_function
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_prompt_execution_settings import OpenAIChatPromptExecutionSettings
+from semantic_kernel.contents import ChatMessageContent, FunctionCallContent, FunctionResultContent, TextContent
+from semantic_kernel.connectors.ai.bedrock.bedrock_prompt_execution_settings import BedrockChatPromptExecutionSettings
+from semantic_kernel.connectors.ai.bedrock.services.bedrock_chat_completion import BedrockChatCompletion
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+#from semantic_kernel.connectors.ai.azure_openai import AzureChatCompletion
+from semantic_kernel.contents import ChatHistory
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+#from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_prompt_execution_settings import OpenAIChatPromptExecutionSettings
+from semantic_kernel.agents import BedrockAgent, BedrockAgentThread
+
 class VectorMemoryRAGPlugin:
     def __init__(self):
         self.text_chunks = []
