68be385f6daa2ae514ec2bbf14eafd037dad84f8
paulmcveigh <paul.mcveigh@kainos.com>
2025-08-05 14:05:07 +0100

    Update failure_score_checker.py

--- src/kernel_functions/failure_score_checker.py
+++ src/kernel_functions/failure_score_checker.py
@@ -1,1 +1,100 @@
+import asyncio
+import os
+import json
+import requests
+import faiss
+import numpy as np
+import sqlite3
+import streamlit as st
+import random
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+
+from dataclasses import dataclass, field, asdict
+from typing import Annotated, Optional, TypedDict, List, Any, Dict
+
+from semantic_kernel import Kernel
+from semantic_kernel.agents import ChatCompletionAgent, ChatHistoryAgentThread
+from semantic_kernel.functions import KernelArguments, kernel_function
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_prompt_execution_settings import OpenAIChatPromptExecutionSettings
+from semantic_kernel.contents import ChatMessageContent, FunctionCallContent, FunctionResultContent, TextContent
+from sentence_transformers import SentenceTransformer
+
+
+
+
+
+import pandas as pd
+#boto3.setup_default_session(profile_name='AWSAdministrator+Bedrock-613820096587')
+
+
+from semantic_kernel.connectors.ai.bedrock.bedrock_prompt_execution_settings import BedrockChatPromptExecutionSettings
+from semantic_kernel.connectors.ai.bedrock.services.bedrock_chat_completion import BedrockChatCompletion
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+#from semantic_kernel.connectors.ai.azure_openai import AzureChatCompletion
+
+from semantic_kernel.contents import ChatHistory
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+#from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_prompt_execution_settings import OpenAIChatPromptExecutionSettings
+
+
+from semantic_kernel.agents import BedrockAgent, BedrockAgentThread
+
+import openai
+
+
+#################################################################
+#TODO: Adapt to use db after chat with Tania
+class FailureScoreChecker:
+    @kernel_function(description="Retrieve the failure score and failure score commentary for an organisation from the database.")
+    async def retrieve_failure_rating(
+        self,
+        claim_data: Annotated[dict, "Structured claim object containing organisation_name."]
+    ) -> dict:
+
+        client = CosmosClient(endpoint, key)
+        database = client.get_database_client(database_name)
+        container = database.get_container_client(container_name)
+
+        items = list(container.read_all_items())
+        print("Items:")
+        print(items)
+        return items
+
+        '''
+        dynamodb = boto3.resource('dynamodb', region_name="eu-north-1")
+        dnb_table = dynamodb.Table("dnb_data")
+        organisation_name = claim_data.get("organisation_name", "N/A")
+        '''
+        '''
+        name = organisation_name
+        df = pd.read_csv("data\\dnb.csv")
+        risk_score = df.loc[df.organization_name==name, 'climate_risk_score'].values[0]
+        return {
+            "organisation_name": name,
+            "climate_risk_score": risk_score
+        }
+        '''
+        '''
+        response = dnb_table.scan()
+        items = response.get('Items', [])
+
+        return items
+        '''
+        '''
+        response = dnb_table.scan(
+            FilterExpression='organization_name = :org',
+            ExpressionAttributeValues={':org': {'S': organisation_name}},
+            ProjectionExpression='organization_name, climate_risk_score'
+        )
+
+        item = response['Items'][0] if response['Items'] else None
+
+        return {
+            "organisation_name": organisation_name,
+            "climate_risk_score": item['climate_risk_score']['S'] if item else None
+        }'''
 
