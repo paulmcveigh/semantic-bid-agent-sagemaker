87f34b71fffc183037de9d5778e481f91eaf2a66
paulmcveigh <paul.mcveigh@kainos.com>
2025-08-07 15:54:49 +0100

    Create interest_rate.py

---
+++ src/kernel_functions/interest_rate.py
@@ -0,0 +1,16 @@
+  @kernel_function(description="Use a model to predict the chance small business has of defaulting on the loan")
+    async def assess_risk(
+        self,
+        claim_data: Annotated[dict, "Structured claim data with fields like coverage_amount and region_of_operation."],
+        risk_score = 100,
+        survival_prob = 0.6
+    ) -> dict:
+
+        base_rate = 0.05  # 5% base
+        risk_adj = min(risk_score * 0.1, 0.10)  # up to +10%
+        survival_adj = max((1 - survival_prob) * 0.05, 0)  # up to +5%
+        return {
+            "interest_rate": round(base_rate + risk_adj + survival_adj, 4),
+            "service_used": self.runtime,
+            "model_used": self.endpoint_name
+        }
