3f636cc54a58783b267c07472793163cd2636896
timjohno <timothy.johnson@kainos.com>
2025-08-07 12:28:01 +0100

    add git ignore and remove imports

--- main.py
+++ main.py
@@ -1,47 +1,25 @@
 import asyncio
-import os
 import json
-import requests
 import faiss
-import numpy as np
-import sqlite3
-import streamlit as st
-import random
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
+from boto3.dynamodb.conditions import Attr
 
 
-from dataclasses import dataclass, field, asdict
-from typing import Annotated, Optional, TypedDict, List, Any, Dict
+from dataclasses import dataclass, field
+from typing import Annotated, Optional, List, Any, Dict
 
 from semantic_kernel import Kernel
 from semantic_kernel.agents import ChatCompletionAgent, ChatHistoryAgentThread
 from semantic_kernel.functions import KernelArguments, kernel_function
-from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
-from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_prompt_execution_settings import OpenAIChatPromptExecutionSettings
-from semantic_kernel.contents import ChatMessageContent, FunctionCallContent, FunctionResultContent, TextContent
+from semantic_kernel.contents import ChatMessageContent, FunctionCallContent, FunctionResultContent
 from sentence_transformers import SentenceTransformer
-
-
-
-
-
-import pandas as pd
-#boto3.setup_default_session(profile_name='AWSAdministrator+Bedrock-613820096587')
 
 
 from semantic_kernel.connectors.ai.bedrock.bedrock_prompt_execution_settings import BedrockChatPromptExecutionSettings
 from semantic_kernel.connectors.ai.bedrock.services.bedrock_chat_completion import BedrockChatCompletion
-from semantic_kernel.contents import ChatHistory
 from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
-#from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_prompt_execution_settings import OpenAIChatPromptExecutionSettings
 
-
-from semantic_kernel.agents import BedrockAgent, BedrockAgentThread
-
-
-#api_key = st.secrets["api_keys"]["OPENAI_API_KEY"]
 
 # --- Instructions
 AGENT_INSTRUCTIONS = """You are an expert insurance underwriting consultant. Your name, if asked, is 'IUA'.
